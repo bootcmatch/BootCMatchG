29522f9232bf0a7e7e99d875a1f6ce05f4d05465
pasquini-dario <44810501+pasquini-dario@users.noreply.github.com>
2021-04-09 16:28:24 +0200

    Update kernel_spgemm_hash_d.cu

--- src/EXTERNAL/nsparse-master/cuda-c/src/kernel/kernel_spgemm_hash_d.cu
+++ src/EXTERNAL/nsparse-master/cuda-c/src/kernel/kernel_spgemm_hash_d.cu
@@ -29,7 +29,7 @@
 /* SpGEMM Specific Parameters */
 #define HASH_SCAL 107 // Set disjoint number to COMP_SH_SIZE
 #define ONSTREAM
-
+#define SHFLMASK 0xFFFFFFFF
 void init_bin(sfBIN *bin, int M)
 {
     int i;
@@ -37,7 +37,7 @@ void init_bin(sfBIN *bin, int M)
     for (i = 0; i < BIN_NUM; i++) {
         cudaStreamCreate(&(bin->stream[i]));
     }
-
+  
     bin->bin_size = (int *)malloc(sizeof(int) * BIN_NUM);
     bin->bin_offset = (int *)malloc(sizeof(int) * BIN_NUM);
     checkCudaErrors(cudaMalloc((void **)&(bin->d_row_perm), sizeof(int) * M));
@@ -118,7 +118,7 @@ __global__ void init_row_perm(int *d_permutation, int M)
     if (i >= M) {
         return;
     }
-
+  
     d_permutation[i] = i;
 }
 
@@ -133,7 +133,7 @@ __global__ void set_row_perm(int *d_bin_size, int *d_bin_offset,
 
     int nz_per_row = d_max_row_nz[i];
     int dest;
-
+  
     int j = 0;
     for (j = 0; j < BIN_NUM - 2; j++) {
         if (nz_per_row <= (min << j)) {
@@ -157,39 +157,34 @@ void set_max_bin(int *d_arpt, int *d_acol, int *d_brpt, sfBIN *bin, int M)
 {
     int i;
     int GS, BS;
-
-
+  
     for (i = 0; i < BIN_NUM; i++) {
         bin->bin_size[i] = 0;
         bin->bin_offset[i] = 0;
     }
-
-    checkCudaErrors(cudaMemcpy(bin->d_bin_size, bin->bin_size, sizeof(int) * BIN_NUM, cudaMemcpyHostToDevice));
-    checkCudaErrors(cudaMemcpy(bin->d_max, &(bin->max_intprod), sizeof(int), cudaMemcpyHostToDevice));
-
-    //checkCudaErrors( cudaMemset(bin->d_bin_size, 0, sizeof(int) * BIN_NUM) );
-    //checkCudaErrors(cudaMemcpy(bin->d_max, &(bin->max_intprod), sizeof(int), cudaMemcpyHostToDevice));
-
+  
+    cudaMemcpy(bin->d_bin_size, bin->bin_size, sizeof(int) * BIN_NUM, cudaMemcpyHostToDevice);
+    cudaMemcpy(bin->d_max, &(bin->max_intprod), sizeof(int), cudaMemcpyHostToDevice);
+  
     BS = 1024;
     GS = div_round_up(M, BS);
     set_intprod_num<<<GS, BS>>>(d_arpt, d_acol, d_brpt, bin->d_row_nz, bin->d_max, M);
-    checkCudaErrors(cudaMemcpy(&(bin->max_intprod), bin->d_max, sizeof(int), cudaMemcpyDeviceToHost));
-
+    cudaMemcpy(&(bin->max_intprod), bin->d_max, sizeof(int), cudaMemcpyDeviceToHost);
+  
     if (bin->max_intprod > IMB_PWMIN) {
         set_bin<<<GS, BS>>>(bin->d_row_nz, bin->d_bin_size, bin->d_max, M, IMB_MIN, IMB_PWMIN);
-
-        checkCudaErrors(cudaMemcpy(bin->bin_size, bin->d_bin_size, sizeof(int) * BIN_NUM, cudaMemcpyDeviceToHost));
-
-        checkCudaErrors(cudaMemcpy(bin->d_bin_size, bin->bin_offset, sizeof(int) * BIN_NUM, cudaMemcpyHostToDevice));
-        //checkCudaErrors( cudaMemset(bin->d_bin_size, 0, sizeof(int) * BIN_NUM) );
+  
+        cudaMemcpy(bin->bin_size, bin->d_bin_size, sizeof(int) * BIN_NUM, cudaMemcpyDeviceToHost);
+        cudaMemcpy(bin->d_bin_size, bin->bin_offset, sizeof(int) * BIN_NUM, cudaMemcpyHostToDevice);
 
         for (i = 0; i < BIN_NUM - 1; i++) {
             bin->bin_offset[i + 1] = bin->bin_offset[i] + bin->bin_size[i];
         }
-        checkCudaErrors(cudaMemcpy(bin->d_bin_offset, bin->bin_offset, sizeof(int) * BIN_NUM, cudaMemcpyHostToDevice));
+        cudaMemcpy(bin->d_bin_offset, bin->bin_offset, sizeof(int) * BIN_NUM, cudaMemcpyHostToDevice);
 
         set_row_perm<<<GS, BS>>>(bin->d_bin_size, bin->d_bin_offset, bin->d_row_nz, bin->d_row_perm, M, IMB_MIN, IMB_PWMIN);
-    }else{
+    }
+    else {
         bin->bin_size[0] = M;
         for (i = 1; i < BIN_NUM; i++) {
             bin->bin_size[i] = 0;
@@ -207,21 +202,21 @@ void set_min_bin(sfBIN *bin, int M)
 {
     int i;
     int GS, BS;
-
+  
     for (i = 0; i < BIN_NUM; i++) {
         bin->bin_size[i] = 0;
         bin->bin_offset[i] = 0;
     }
-
+  
     cudaMemcpy(bin->d_bin_size, bin->bin_size, sizeof(int) * BIN_NUM, cudaMemcpyHostToDevice);
     cudaMemcpy(bin->d_max, &(bin->max_nz), sizeof(int), cudaMemcpyHostToDevice);
-
+  
     BS = 1024;
     GS = div_round_up(M, BS);
     set_bin<<<GS, BS>>>(bin->d_row_nz, bin->d_bin_size,
                         bin->d_max,
                         M, B_MIN, B_PWMIN);
-
+  
     cudaMemcpy(&(bin->max_nz), bin->d_max, sizeof(int), cudaMemcpyDeviceToHost);
     if (bin->max_nz > B_PWMIN) {
         cudaMemcpy(bin->bin_size, bin->d_bin_size, sizeof(int) * BIN_NUM, cudaMemcpyDeviceToHost);
@@ -231,7 +226,7 @@ void set_min_bin(sfBIN *bin, int M)
             bin->bin_offset[i + 1] = bin->bin_offset[i] + bin->bin_size[i];
         }
         cudaMemcpy(bin->d_bin_offset, bin->bin_offset, sizeof(int) * BIN_NUM, cudaMemcpyHostToDevice);
-
+  
         set_row_perm<<<GS, BS>>>(bin->d_bin_size, bin->d_bin_offset, bin->d_row_nz, bin->d_row_perm, M, B_MIN, B_PWMIN);
     }
 
@@ -274,19 +269,19 @@ __global__ void set_row_nz_bin_pwarp(const int *d_arpt, const int *d_acol,
                                      const int *d_row_perm,
                                      int *d_row_nz,
                                      int bin_offset, int M) {
-
+  
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int rid = i / PWARP;
     int tid = i % PWARP;
     int local_rid = rid % (blockDim.x / PWARP);
-
+  
     int j, k;
     int soffset;
     int acol, bcol, key, hash, adr, nz, old;
     __shared__ int check[IMB_PW_SH_SIZE];
-
+  
     soffset = local_rid * IMB_PWMIN;
-
+  
     for (j = tid; j < IMB_PWMIN; j += PWARP) {
         check[soffset + j] = -1;
     }
@@ -321,9 +316,9 @@ __global__ void set_row_nz_bin_pwarp(const int *d_arpt, const int *d_acol,
             }
         }
     }
-
+  
     for (j = PWARP / 2; j >= 1; j /= 2) {
-        nz += __shfl_xor(nz, j);
+        nz += __shfl_xor_sync(SHFLMASK,nz, j);
     }
 
     if (tid == 0) {
@@ -355,11 +350,11 @@ __global__ void set_row_nz_bin_each(const int *d_arpt, const int *d_acol,
     for (j = tid; j < SH_ROW; j += WARP) {
         check[soffset + j] = -1;
     }
-
+  
     if (rid >= M) {
         return;
     }
-
+  
     acol = 0;
     nz = 0;
     rid = d_row_perm[rid + bin_offset];
@@ -421,11 +416,11 @@ __global__ void set_row_nz_bin_each_tb(const int *d_arpt, const int *d_acol,
     for (j = threadIdx.x; j < SH_ROW; j += blockDim.x) {
         check[j] = -1;
     }
-
+  
     if (rid >= M) {
         return;
     }
-
+  
     __syncthreads();
 
     nz = 0;
@@ -457,9 +452,9 @@ __global__ void set_row_nz_bin_each_tb(const int *d_arpt, const int *d_acol,
     }
 
     for (j = WARP / 2; j >= 1; j /= 2) {
-        nz += __shfl_xor(nz, j);
+        nz += __shfl_xor_sync(SHFLMASK,nz, j);
     }
-
+  
     __syncthreads();
     if (threadIdx.x == 0) {
         check[0] = 0;
@@ -470,7 +465,7 @@ __global__ void set_row_nz_bin_each_tb(const int *d_arpt, const int *d_acol,
         atomicAdd(check, nz);
     }
     __syncthreads();
-
+  
     if (threadIdx.x == 0) {
         d_row_nz[rid] = check[0];
     }
@@ -501,13 +496,13 @@ __global__ void set_row_nz_bin_each_tb_large(const int *d_arpt, const int *d_aco
     if (threadIdx.x == 0) {
         snz[0] = 0;
     }
-
+  
     if (rid >= M) {
         return;
     }
-
+  
     __syncthreads();
-
+  
     rid = d_row_perm[rid + bin_offset];
     int count = 0;
     int border = SH_ROW >> 1;
@@ -543,7 +538,7 @@ __global__ void set_row_nz_bin_each_tb_large(const int *d_arpt, const int *d_aco
             break;
         }
     }
-
+  
     __syncthreads();
     if (count >= border || snz[0] >= border) {
         if (threadIdx.x == 0) {
@@ -580,11 +575,11 @@ __global__ void set_row_nz_bin_each_gl(const int *d_arpt, const int *d_acol,
         snz[0] = 0;
     }
     __syncthreads();
-
+  
     if (rid >= M) {
         return;
     }
-
+  
     nz = 0;
     rid = d_row_perm[rid + bin_offset];
     for (j = d_arpt[rid] + wid; j < d_arpt[rid + 1]; j += wnum) {
@@ -612,11 +607,11 @@ __global__ void set_row_nz_bin_each_gl(const int *d_arpt, const int *d_acol,
             }
         }
     }
-
+  
     for (j = WARP / 2; j >= 1; j /= 2) {
-        nz += __shfl_xor(nz, j);
+        nz += __shfl_xor_sync(SHFLMASK,nz, j);
     }
-
+  
     if (tid == 0) {
         atomicAdd(snz, nz);
     }
@@ -631,7 +626,7 @@ void set_row_nnz(int *d_arpt, int *d_acol,
                  int *d_crpt,
                  sfBIN *bin,
                  int M, int *nnz);
-
+  
 
 __global__ void calculate_value_col_bin_pwarp(const int *d_arpt,
                                               const int *d_acol,
@@ -646,7 +641,7 @@ __global__ void calculate_value_col_bin_pwarp(const int *d_arpt,
                                               int *d_nz,
                                               int bin_offset,
                                               int bin_size) {
-
+  
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int rid = i / PWARP;
     int tid = i % PWARP;
@@ -654,19 +649,19 @@ __global__ void calculate_value_col_bin_pwarp(const int *d_arpt,
     int j;
     __shared__ int shared_check[B_PW_SH_SIZE];
     __shared__ real shared_value[B_PW_SH_SIZE];
-
+  
     int soffset = local_rid * (B_PWMIN);
-
+  
     for (j = tid; j < (B_PWMIN); j += PWARP) {
         shared_check[soffset + j] = -1;
         shared_value[soffset + j] = 0;
     }
-
+  
     if (rid >= bin_size) {
         return;
     }
     rid = d_row_perm[rid + bin_offset];
-
+  
     if (tid == 0) {
         d_nz[rid] = 0;
     }
@@ -682,7 +677,7 @@ __global__ void calculate_value_col_bin_pwarp(const int *d_arpt,
         for (k = d_brpt[acol]; k < d_brpt[acol + 1]; k++) {
             bcol = d_bcol[k];
             bval = d_bval[k];
-
+	
             key = bcol;
             hash = (bcol * HASH_SCAL) & ((B_PWMIN) - 1);
             adr = soffset + hash;
@@ -705,7 +700,7 @@ __global__ void calculate_value_col_bin_pwarp(const int *d_arpt,
             }
         }
     }
-
+  
     for (j = tid; j < (B_PWMIN); j += PWARP) {
         if (shared_check[soffset + j] != -1) {
             index = atomicAdd(d_nz + rid, 1);
@@ -750,14 +745,14 @@ __global__ void calculate_value_col_bin_each(const int *d_arpt,
     int j;
     __shared__ int shared_check[B_SH_SIZE];
     __shared__ real shared_value[B_SH_SIZE];
-
+  
     int soffset = wid * SH_ROW;
 
     for (j = tid; j < SH_ROW; j += WARP) {
         shared_check[soffset + j] = -1;
         shared_value[soffset + j] = 0;
     }
-
+  
     if (rid >= bin_size) {
         return;
     }
@@ -785,7 +780,7 @@ __global__ void calculate_value_col_bin_each(const int *d_arpt,
             for (k = d_brpt[acol] + tid; k < d_brpt[acol + 1]; k += WARP) {
                 bcol = d_bcol[k];
                 bval = d_bval[k];
-
+	
                 key = bcol;
                 hash = (bcol * HASH_SCAL) & (SH_ROW - 1);
                 adr = soffset + hash;
@@ -809,7 +804,7 @@ __global__ void calculate_value_col_bin_each(const int *d_arpt,
             }
         }
     }
-
+  
     for (j = tid; j < SH_ROW; j += WARP) {
         if (shared_check[soffset + j] != -1) {
             index = atomicAdd(d_nz + rid, 1);
@@ -853,12 +848,12 @@ __global__ void calculate_value_col_bin_each_tb(const int *d_arpt,
     int j;
     __shared__ int shared_check[SH_ROW];
     __shared__ real shared_value[SH_ROW];
-
+  
     for (j = threadIdx.x; j < SH_ROW; j += blockDim.x) {
         shared_check[j] = -1;
         shared_value[j] = 0;
     }
-
+  
     if (rid >= bin_size) {
         return;
     }
@@ -883,7 +878,7 @@ __global__ void calculate_value_col_bin_each_tb(const int *d_arpt,
         for (k = d_brpt[acol] + tid; k < d_brpt[acol + 1]; k += WARP) {
             bcol = d_bcol[k];
             bval = d_bval[k];
-
+	
             key = bcol;
             hash = (bcol * HASH_SCAL) & (SH_ROW - 1);
             while (1) {
@@ -904,7 +899,7 @@ __global__ void calculate_value_col_bin_each_tb(const int *d_arpt,
             }
         }
     }
-
+  
     __syncthreads();
     if (threadIdx.x < WARP) {
         for (j = tid; j < SH_ROW; j += WARP) {
@@ -953,7 +948,7 @@ __global__ void calculate_value_col_bin_each_gl(const int *d_arpt,
     int wid = threadIdx.x / WARP;
     int wnum = blockDim.x / WARP;
     int j;
-
+  
     if (rid >= M) {
         return;
     }
@@ -961,7 +956,7 @@ __global__ void calculate_value_col_bin_each_gl(const int *d_arpt,
     int doffset = rid * max_row_nz;
 
     rid = d_row_perm[rid + bin_offset];
-
+  
     if (threadIdx.x == 0) {
         d_nz[rid] = 0;
     }
@@ -980,7 +975,7 @@ __global__ void calculate_value_col_bin_each_gl(const int *d_arpt,
         for (k = d_brpt[acol] + tid; k < d_brpt[acol + 1]; k += WARP) {
             bcol = d_bcol[k];
             bval = d_bval[k];
-
+      
             key = bcol;
             hash = (bcol * HASH_SCAL) % max_row_nz;
             adr = doffset + hash;
@@ -1003,7 +998,7 @@ __global__ void calculate_value_col_bin_each_gl(const int *d_arpt,
             }
         }
     }
-
+  
     __syncthreads();
     if (threadIdx.x < WARP) {
         for (j = tid; j < max_row_nz; j += WARP) {
@@ -1016,7 +1011,7 @@ __global__ void calculate_value_col_bin_each_gl(const int *d_arpt,
     }
     __syncthreads();
     int nz = d_nz[rid];
-
+  
     /* Sorting for shared data */
     int count, target;
     for (j = threadIdx.x; j < nz; j += blockDim.x) {
@@ -1036,27 +1031,23 @@ void calculate_value_col_bin(int *d_arpt, int *d_acol, real *d_aval,
                              int *d_crpt, int *d_ccol, real *d_cval,
                              sfBIN *bin,
                              int M);
-
+  
 void spgemm_kernel_hash(sfCSR *a, sfCSR *b, sfCSR *c)
 {
-
+  
     int M;
     sfBIN bin;
-
+  
     M = a->M;
     c->M = M;
     c->N = b->N;
-
+  
     /* Initialize bin */
     init_bin(&bin, M);
 
-    checkCudaErrors(cudaDeviceSynchronize());
-
     /* Set max bin */
     set_max_bin(a->d_rpt, a->d_col, b->d_rpt, &bin, M);
-
-    checkCudaErrors(cudaDeviceSynchronize());
-
+  
     checkCudaErrors(cudaMalloc((void **)&(c->d_rpt), sizeof(int) * (M + 1)));
 
     /* Count nz of C */
@@ -1069,10 +1060,10 @@ void spgemm_kernel_hash(sfCSR *a, sfCSR *b, sfCSR *c)
 
     /* Set bin */
     set_min_bin(&bin, M);
-
+  
     checkCudaErrors(cudaMalloc((void **)&(c->d_col), sizeof(int) * c->nnz));
     checkCudaErrors(cudaMalloc((void **)&(c->d_val), sizeof(real) * c->nnz));
-
+  
     /* Calculating value of C */
     calculate_value_col_bin(a->d_rpt, a->d_col, a->d_val,
                             b->d_rpt, b->d_col, b->d_val,
